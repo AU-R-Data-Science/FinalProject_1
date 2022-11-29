97b06c30d1406fda70f03034c5b5a11a74987761
Kaley Fulcher <fulcher.kaley@gmail.com>
2022-11-29 10:06:35 -0600

    Help Documentation and Description
    Help documentation for optimization function

--- R/hello.R
+++ R/hello.R
@@ -8,6 +8,38 @@
 #   Test Package:              'Cmd + Shift + T'
 
 
+
+#' Estimate logistic model via optimization
+#' @description This function generates the outputs listed in the final project description.  First, it calculates
+#' the initial values for optimization obtained from the least-sqaures formula.  Next, it optimizes the values using
+#' numerical optimization.  After this, it plots the fitted logistic curve to the actual values. Also, it outputs
+#' Prevalence, Accuracy, Sensitivity, Specificity, False Discovery Rate, Diagnostic Odds Ratio.
+#'
+#' @param y A \code{double} value of the 1 column matrix containing the response of interest.
+#' @param X An \eqn{n \times p} \code{double} value of the matrix containing the values of the predictors.
+#' @author
+#' @export
+logisticRegression <- function(y, X) {
+
+  int <- rep(1, nrow=nrow(X))
+  datatoTest$X <- cbind(int, datatoTest$X)
+
+  print("Initial values for optimization obtained from the least-squares formula: ")
+  write.table(leastSquares(datatoTest$y, datatoTest$X), col.names = F)
+  cat("\n\n")
+
+
+  output <- beta_hat(datatoTest$y, datatoTest$X)$beta_hat
+  print("Values after optimization: ")
+  write.table(output, col.names = F)
+  cat("\n\n")
+  plotRegressionCurves(y, X)
+  confusionMatrix(y, X)
+
+}
+
+
+
 leastSquares <- function(y, X) {
   solve(t(X) %*% X) %*% t(X) %*% y
 }
@@ -198,9 +230,9 @@ getDiagnosticOddsRatio <- function(TP, TN, FN, FP) {
 #' @export
 plotMetric <- function(y, X, metric) {
   output <- beta_hat(y, X)$beta_hat
-  
+
   whichMetric <- tolower(metric)
-  
+
   if (whichMetric == "prevalence") {
     cutOffArr <- c(.1, .2, .3, .4, .5, .6, .7, .8, .9)
     metricVals <- c()
@@ -208,12 +240,12 @@ plotMetric <- function(y, X, metric) {
       predBeforeRound <- 1/(1+exp(-X%*%output))
       pred[predBeforeRound <= cutOffArr[d]] <- 0
       pred[predBeforeRound > cutOffArr[d]] <- 1
-      
+
       TP <- 0
       TN <- 0
       FN <- 0
       FP <- 0
-      
+
       for (i in 1:nrow(y)) {
         if (y[i] == 1 && pred[i] == 1) {
           TP <- TP + 1
@@ -225,19 +257,19 @@ plotMetric <- function(y, X, metric) {
         else if (y[i] == 0 && pred[i] == 1) {
           FP <- FP + 1
         }
-        
+
       }
       metricVals <- c(metricVals, getPrevalence(TP, TN, FN, FP))
     }
-    
+
     df <- data.frame(CutOff=cutOffArr,
                      Prevalence=metricVals)
-    
+
     p<-ggplot(data=df, aes(x=CutOff, y=Prevalence)) +
       geom_bar(stat="identity")
     p
-    
-    
+
+
   } else if (whichMetric == "accuracy") {
     cutOffArr <- c(.1, .2, .3, .4, .5, .6, .7, .8, .9)
     metricVals <- c()
@@ -245,12 +277,12 @@ plotMetric <- function(y, X, metric) {
       predBeforeRound <- 1/(1+exp(-X%*%output))
       pred[predBeforeRound <= cutOffArr[d]] <- 0
       pred[predBeforeRound > cutOffArr[d]] <- 1
-      
+
       TP <- 0
       TN <- 0
       FN <- 0
       FP <- 0
-      
+
       for (i in 1:nrow(y)) {
         if (y[i] == 1 && pred[i] == 1) {
           TP <- TP + 1
@@ -262,19 +294,19 @@ plotMetric <- function(y, X, metric) {
         else if (y[i] == 0 && pred[i] == 1) {
           FP <- FP + 1
         }
-        
+
       }
       metricVals <- c(metricVals, getAccuracy(TP, TN, FN, FP))
     }
-    
+
     df <- data.frame(CutOff=cutOffArr,
                      Accuracy=metricVals)
-    
+
     p<-ggplot(data=df, aes(x=CutOff, y=Accuracy)) +
       geom_bar(stat="identity")
     p
-    
-    
+
+
   } else if  (whichMetric == "sensitivity") {
     cutOffArr <- c(.1, .2, .3, .4, .5, .6, .7, .8, .9)
     metricVals <- c()
@@ -282,12 +314,12 @@ plotMetric <- function(y, X, metric) {
       predBeforeRound <- 1/(1+exp(-X%*%output))
       pred[predBeforeRound <= cutOffArr[d]] <- 0
       pred[predBeforeRound > cutOffArr[d]] <- 1
-      
+
       TP <- 0
       TN <- 0
       FN <- 0
       FP <- 0
-      
+
       for (i in 1:nrow(y)) {
         if (y[i] == 1 && pred[i] == 1) {
           TP <- TP + 1
@@ -299,18 +331,18 @@ plotMetric <- function(y, X, metric) {
         else if (y[i] == 0 && pred[i] == 1) {
           FP <- FP + 1
         }
-        
+
       }
       metricVals <- c(metricVals, getSensitivity(TP, TN, FN, FP))
     }
-    
+
     df <- data.frame(CutOff=cutOffArr,
                      Sensitivity=metricVals)
-    
+
     p<-ggplot(data=df, aes(x=CutOff, y=Sensitivity)) +
       geom_bar(stat="identity")
     p
-    
+
   } else if (whichMetric == "specificity") {
     cutOffArr <- c(.1, .2, .3, .4, .5, .6, .7, .8, .9)
     metricVals <- c()
@@ -318,12 +350,12 @@ plotMetric <- function(y, X, metric) {
       predBeforeRound <- 1/(1+exp(-X%*%output))
       pred[predBeforeRound <= cutOffArr[d]] <- 0
       pred[predBeforeRound > cutOffArr[d]] <- 1
-      
+
       TP <- 0
       TN <- 0
       FN <- 0
       FP <- 0
-      
+
       for (i in 1:nrow(y)) {
         if (y[i] == 1 && pred[i] == 1) {
           TP <- TP + 1
@@ -335,19 +367,19 @@ plotMetric <- function(y, X, metric) {
         else if (y[i] == 0 && pred[i] == 1) {
           FP <- FP + 1
         }
-        
+
       }
       metricVals <- c(metricVals, getSpecificity(TP, TN, FN, FP))
     }
-    
+
     df <- data.frame(CutOff=cutOffArr,
                      Specificity=metricVals)
-    
+
     p<-ggplot(data=df, aes(x=CutOff, y=Specificity)) +
       geom_bar(stat="identity")
     p
-    
-    
+
+
   } else if (whichMetric == "false discovery rate") {
     cutOffArr <- c(.1, .2, .3, .4, .5, .6, .7, .8, .9)
     metricVals <- c()
@@ -355,12 +387,12 @@ plotMetric <- function(y, X, metric) {
       predBeforeRound <- 1/(1+exp(-X%*%output))
       pred[predBeforeRound <= cutOffArr[d]] <- 0
       pred[predBeforeRound > cutOffArr[d]] <- 1
-      
+
       TP <- 0
       TN <- 0
       FN <- 0
       FP <- 0
-      
+
       for (i in 1:nrow(y)) {
         if (y[i] == 1 && pred[i] == 1) {
           TP <- TP + 1
@@ -372,19 +404,19 @@ plotMetric <- function(y, X, metric) {
         else if (y[i] == 0 && pred[i] == 1) {
           FP <- FP + 1
         }
-        
+
       }
       metricVals <- c(metricVals, getFalseDiscoveryRate(TP, TN, FN, FP))
     }
-    
+
     df <- data.frame(CutOff=cutOffArr,
                      FalseDiscoveryRate=metricVals)
-    
+
     p<-ggplot(data=df, aes(x=CutOff, y=FalseDiscoveryRate)) +
       geom_bar(stat="identity")
     p
-    
-    
+
+
   } else if (whichMetric == "Diagnostic Odds Ratio") {
     cutOffArr <- c(.1, .2, .3, .4, .5, .6, .7, .8, .9)
     metricVals <- c()
@@ -392,12 +424,12 @@ plotMetric <- function(y, X, metric) {
       predBeforeRound <- 1/(1+exp(-X%*%output))
       pred[predBeforeRound <= cutOffArr[d]] <- 0
       pred[predBeforeRound > cutOffArr[d]] <- 1
-      
+
       TP <- 0
       TN <- 0
       FN <- 0
       FP <- 0
-      
+
       for (i in 1:nrow(y)) {
         if (y[i] == 1 && pred[i] == 1) {
           TP <- TP + 1
@@ -409,25 +441,24 @@ plotMetric <- function(y, X, metric) {
         else if (y[i] == 0 && pred[i] == 1) {
           FP <- FP + 1
         }
-        
+
       }
       metricVals <- c(metricVals, getDiagnosticOddsRatio(TP, TN, FN, FP))
     }
-    
+
     df <- data.frame(CutOff=cutOffArr,
                      DiagnosticOddsRatio=metricVals)
-    
+
     p<-ggplot(data=df, aes(x=CutOff, y=DiagnosticOddsRatio)) +
       geom_bar(stat="identity")
     p
-    
-    
+
+
   } else {
     print("Invalid metric.. Must be one of the following..")
   }
-  
-  
-}
 
+
+}
 
 
